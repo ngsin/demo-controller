281502897e294c1369283901d12fafcde4f385d7
Xiansong Wu <wuxiansong0125@gmail.com>
2023-07-11 21:12:02 +0800

    feat: update

--- controller.go
+++ controller.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/ngsin/demo-controller/pkg/apis/demoapi/v1alpha1"
 	demoClients "github.com/ngsin/demo-controller/pkg/client/clientset/versioned"
 	demoScheme "github.com/ngsin/demo-controller/pkg/client/clientset/versioned/scheme"
 	demoAPIInformer "github.com/ngsin/demo-controller/pkg/client/informers/externalversions/demoapi/v1alpha1"
 	demoAPILister "github.com/ngsin/demo-controller/pkg/client/listers/demoapi/v1alpha1"
+	appsV1 "k8s.io/api/apps/v1"
 	coreV1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -95,23 +99,12 @@ func NewController(
 	klog.Info("Setting up event handlers")
 	// Set up an event handler for when Demo resources change
 	demoAPIInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			klog.Info("add demoAPI")
-			controller.recorder.Event(obj.(runtime.Object), coreV1.EventTypeNormal, "demoAPI", "AddFunc")
-
-		},
+		AddFunc: controller.enqueueDemo,
 		UpdateFunc: func(oldObj, newObj interface{}) {
-			klog.Info("update demoAPI")
-			controller.recorder.Event(oldObj.(runtime.Object), coreV1.EventTypeNormal, "demoAPI", "AddFunc")
-			klog.Infof("update demoAPI: %v", newObj.(*v1alpha1.DemoAPI).Spec.Image)
-			time.Sleep(10 * time.Second)
+			controller.enqueueDemo(newObj)
 
 		},
-		DeleteFunc: func(obj interface{}) {
-			klog.Info("delete demoAPI")
-			controller.recorder.Event(obj.(runtime.Object), coreV1.EventTypeNormal, "demoAPI", "DeleteFunc")
-
-		},
+		DeleteFunc: controller.enqueueDemo,
 	})
 
 	deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
@@ -126,7 +119,6 @@ func NewController(
 		DeleteFunc: func(obj interface{}) {
 			klog.Info("delete deployment")
 			controller.recorder.Event(obj.(runtime.Object), coreV1.EventTypeNormal, "deployment", "DeleteFunc")
-
 		},
 	})
 
@@ -164,13 +156,14 @@ func (c *Controller) Run(workers int, stopCh <-chan struct{}) error {
 // processNextWorkItem function in order to read and process a message on the
 // workqueue.
 func (c *Controller) runWorker() {
-	klog.Info("runWorker")
+	for c.processNextWorkItem() {
+	}
 }
 
 // enqueueFoo takes a Foo resource and converts it into a namespace/name
 // string which is then put onto the work queue. This method should *not* be
 // passed resources of any type other than Foo.
-func (c *Controller) enqueueFoo(obj interface{}) {
+func (c *Controller) enqueueDemo(obj interface{}) {
 	var key string
 	var err error
 	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
@@ -178,4 +171,192 @@ func (c *Controller) enqueueFoo(obj interface{}) {
 		return
 	}
 	c.workqueue.Add(key)
+}
+
+func (c *Controller) processNextWorkItem() bool {
+	obj, shutdown := c.workqueue.Get()
+
+	if shutdown {
+		return false
+	}
+
+	// We wrap this block in a func so we can defer c.workqueue.Done.
+	err := func(obj interface{}) error {
+		// We call Done here so the workqueue knows we have finished
+		// processing this item. We also must remember to call Forget if we
+		// do not want this work item being re-queued. For example, we do
+		// not call Forget if a transient error occurs, instead the item is
+		// put back on the workqueue and attempted again after a back-off
+		// period.
+		defer c.workqueue.Done(obj)
+		var key string
+		var ok bool
+		// We expect strings to come off the workqueue. These are of the
+		// form namespace/name. We do this as the delayed nature of the
+		// workqueue means the items in the informer cache may actually be
+		// more up to date that when the item was initially put onto the
+		// workqueue.
+		if key, ok = obj.(string); !ok {
+			// As the item in the workqueue is actually invalid, we call
+			// Forget here else we'd go into a loop of attempting to
+			// process a work item that is invalid.
+			c.workqueue.Forget(obj)
+			utilruntime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
+			return nil
+		}
+		// Run the syncHandler, passing it the namespace/name string of the
+		// Foo resource to be synced.
+		if err := c.syncHandler(key); err != nil {
+			// Put the item back on the workqueue to handle any transient errors.
+			c.workqueue.AddRateLimited(key)
+			return fmt.Errorf("error syncing '%s': %s, requeuing", key, err.Error())
+		}
+		// Finally, if no error occurs we Forget this item so it does not
+		// get queued again until another change happens.
+		c.workqueue.Forget(obj)
+		klog.Infof("Successfully synced '%s'", key)
+		return nil
+	}(obj)
+
+	if err != nil {
+		utilruntime.HandleError(err)
+		return true
+	}
+
+	return true
+
+}
+
+// syncHandler compares the actual state with the desired, and attempts to
+// converge the two. It then updates the Status block of the Foo resource
+// with the current status of the resource.
+func (c *Controller) syncHandler(key string) error {
+	// Convert the namespace/name string into a distinct namespace and name
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		return nil
+	}
+
+	// Get the Foo resource with this namespace/name
+	demo, err := c.demoAPIsLister.DemoAPIs(namespace).Get(name)
+	if err != nil {
+		// The Foo resource may no longer exist, in which case we stop
+		// processing.
+		if errors.IsNotFound(err) {
+			utilruntime.HandleError(fmt.Errorf("foo '%s' in work queue no longer exists", key))
+			return nil
+		}
+
+		return err
+	}
+
+	deploymentName := demo.Spec.DeploymentName
+	if deploymentName == "" {
+		// We choose to absorb the error here as the worker would requeue the
+		// resource otherwise. Instead, the next time the resource is updated
+		// the resource will be queued again.
+		utilruntime.HandleError(fmt.Errorf("%s: deployment name must be specified", key))
+		return nil
+	}
+
+	// Get the deployment with the name specified in Foo.spec
+	deployment, err := c.deploymentsLister.Deployments(demo.Namespace).Get(deploymentName)
+	// If the resource doesn't exist, we'll create it
+	if errors.IsNotFound(err) {
+		deployment, err = c.kubeClientSet.AppsV1().Deployments(demo.Namespace).Create(context.TODO(), newDeployment(demo), metaV1.CreateOptions{})
+	}
+
+	// If an error occurs during Get/Create, we'll requeue the item so we can
+	// attempt processing again later. This could have been caused by a
+	// temporary network failure, or any other transient reason.
+	if err != nil {
+		return err
+	}
+
+	// If the Deployment is not controlled by this Foo resource, we should log
+	// a warning to the event recorder and return error msg.
+	if !metaV1.IsControlledBy(deployment, demo) {
+		msg := fmt.Sprintf(MessageResourceExists, deployment.Name)
+		c.recorder.Event(demo, coreV1.EventTypeWarning, ErrResourceExists, msg)
+		return fmt.Errorf("%s", msg)
+	}
+
+	// If this number of the replicas on the Foo resource is specified, and the
+	// number does not equal the current desired replicas on the Deployment, we
+	// should update the Deployment resource.
+	if demo.Spec.Replicas != nil && *demo.Spec.Replicas != *deployment.Spec.Replicas {
+		klog.V(4).Infof("Foo %s replicas: %d, deployment replicas: %d", name, *demo.Spec.Replicas, *deployment.Spec.Replicas)
+		deployment, err = c.kubeClientSet.AppsV1().Deployments(demo.Namespace).Update(context.TODO(), newDeployment(demo), metaV1.UpdateOptions{})
+	}
+
+	// If an error occurs during Update, we'll requeue the item so we can
+	// attempt processing again later. This could have been caused by a
+	// temporary network failure, or any other transient reason.
+	if err != nil {
+		return err
+	}
+
+	// Finally, we update the status block of the Foo resource to reflect the
+	// current state of the world
+	err = c.updateDemoStatus(demo, deployment)
+	if err != nil {
+		return err
+	}
+
+	c.recorder.Event(demo, coreV1.EventTypeNormal, SuccessSynced, MessageResourceSynced)
+	return nil
+}
+
+func (c *Controller) updateDemoStatus(demo *v1alpha1.DemoAPI, deployment *appsV1.Deployment) error {
+	// NEVER modify objects from the store. It's a read-only, local cache.
+	// You can use DeepCopy() to make a deep copy of original object and modify this copy
+	// Or create a copy manually for better performance
+	demoCopy := demo.DeepCopy()
+	demoCopy.Status.AvailableReplicas = deployment.Status.AvailableReplicas
+	// If the CustomResourceSubresources feature gate is not enabled,
+	// we must use Update instead of UpdateStatus to update the Status block of the Foo resource.
+	// UpdateStatus will not allow changes to the Spec of the resource,
+	// which is ideal for ensuring nothing other than resource status has been updated.
+	_, err := c.demoClientSet.DemogroupV1alpha1().DemoAPIs(demo.Namespace).UpdateStatus(context.TODO(), demoCopy, metaV1.UpdateOptions{})
+	return err
+}
+
+// newDeployment creates a new Deployment for a Foo resource. It also sets
+// the appropriate OwnerReferences on the resource so handleObject can discover
+// the Foo resource that 'owns' it.
+func newDeployment(demo *v1alpha1.DemoAPI) *appsV1.Deployment {
+	labels := map[string]string{
+		"app":        "nginx",
+		"controller": demo.Name,
+	}
+
+	return &appsV1.Deployment{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      demo.Spec.DeploymentName,
+			Namespace: demo.Namespace,
+			OwnerReferences: []metaV1.OwnerReference{
+				*metaV1.NewControllerRef(demo, v1alpha1.SchemeGroupVersion.WithKind("DemoAPI")),
+			},
+		},
+		Spec: appsV1.DeploymentSpec{
+			Replicas: demo.Spec.Replicas,
+			Selector: &metaV1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: coreV1.PodTemplateSpec{
+				ObjectMeta: metaV1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: coreV1.PodSpec{
+					Containers: []coreV1.Container{
+						{
+							Name:  "demo-container",
+							Image: demo.Spec.Image,
+						},
+					},
+				},
+			},
+		},
+	}
 }
